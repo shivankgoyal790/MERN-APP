966cdb13f1459c35379af5864f61c3f66f00c6b9
shivank goyal <goyal.shivank790@gmail.com>
2021-06-23 17:26:54 +0530

    css changed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react"
+import { BrowserRouter as Router, Route , Switch, Redirect } from "react-router-dom";
+import Users from "./Users/Users";
+import NewPlace from "./Places/Newplace";
+import MainNavigation from "./Shared/Components/MainNavigation";
+import Auth from "./Shared/Auth"
 function App() {
   return (
-    <div className="App">
-      <h1>bye</h1>
-    </div>
+
+    <Router>
+      <MainNavigation />
+      <Switch>
+          <Route path="/" exact><Users /></Route>
+          <Route path="/myplaces/new" exact><NewPlace /></Route>
+          <Route path="/Auth" exact><Auth /></Route>
+          <Redirect to="/" />
+      </Switch>
+    </Router>
+  
   );
 }
 
