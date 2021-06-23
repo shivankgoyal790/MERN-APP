131a551c05d9e7d76be9ae070103eec69c673e36
shivank goyal <goyal.shivank790@gmail.com>
2021-06-23 17:24:56 +0530

    all nav linked added to router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import './index.css';
-import Users from "./Users/Users"
 ReactDOM.render(
-      <Users />,document.getElementById('root')
+      <App />,document.getElementById('root')
 );
 
