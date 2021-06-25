583c12c049ada1a79ebd1691d34d64317afc5813
shivank goyal <goyal.shivank790@gmail.com>
2021-06-25 22:52:35 +0530

    side drawer menu added

--- src/Shared/Components/Sidedrawer.js
+++ src/Shared/Components/Sidedrawer.js
@@ -1,19 +1,19 @@
 import React from "react"
+import ReactDOM from "react-dom"
 import "./Sidedrawer.css"
-
-const Sidedrawer = () =>{
-   return(
-            <div className="sidebackground">
-                <div className="drawer">
+import {Link} from "react-router-dom"
+const Sidedrawer = (props) =>{
+   return ReactDOM.createPortal(
+                <div className="drawer" style={{display:props.show ? "block" : "none"}} onClick={props.onClick}>
                     <div className="drawerlist">
-                        <div className="drawernav">USERS</div>
-                        <div className="drawernav">PLACES</div>
-                        <div className="drawernav">ADD PLACES</div>
-                        <div className="drawernav">LOGIN/SIGNUP</div>
+                        <div className="drawernav"><Link to="/">USERS</Link></div>
+                        <div className="drawernav"><Link to="/u1/myplaces">PLACES</Link></div>
+                        <div className="drawernav"><Link to="/myplaces/new">ADD PLACES</Link></div>
+                        <div className="drawernav"><Link to="/auth">LOGIN/SIGNUP</Link></div>
                     </div>
-                </div>
-            </div>
-   ) ;   
+                </div>,document.getElementById('sidedrawer-hook')
+               
+   );   
 }
 
 export default Sidedrawer;
