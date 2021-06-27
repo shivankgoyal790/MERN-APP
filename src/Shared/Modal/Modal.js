792b684f68a936b867df5712d51e2c083496f223
shivank goyal <goyal.shivank790@gmail.com>
2021-06-27 23:29:46 +0530

    responsive modal created

--- src/Shared/Modal/Modal.js
+++ src/Shared/Modal/Modal.js
@@ -1,16 +1,16 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import "./Modal.css"
 import Logo1 from "../images/google-map.jpg"
 const Modal = (props) =>{
-    return(
+    return ReactDOM.createPortal(
             <div className="modal-container" style={{display : props.show ? "block" : "none" }}>
                 <h1 className="modal-header">{props.location}</h1>
                 <div className="map-container">
                     <img src={Logo1} alt="map" />
                     <button className="map-button" onClick={props.onClick}>CLOSE</button>
                 </div>
-            </div>
-    );
+            </div>,document.getElementById("modal-hook"));
 }
-
+    
 export default Modal
