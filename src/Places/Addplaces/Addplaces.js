179f777629ae9aa23c07f2e04607bf804a36f34b
shivank goyal <goyal.shivank790@gmail.com>
2021-06-30 23:08:11 +0530

    filterd the places according creator

--- src/Places/Addplaces/Addplaces.js
+++ src/Places/Addplaces/Addplaces.js
@@ -1,20 +1,79 @@
-import React from "react"
+import React, { useState } from "react"
 import "./Addplaces.css"
 
 const Addplaces = () =>{
-    return(
+
+    const [Newvalue , ChangeNewvalue ] = useState({
+            country: "",
+            address : "",
+            location : ""}
+        )
+
+    const InputHandler = (event) =>{
+        const name = event.target.name;
+        const value = event.target.value;
+        ChangeNewvalue((prev) =>{
+            if(name === "country")
+             {return{
+                country : value,
+                address : prev.address,
+                location : prev.location
+             }}
+             if(name === "building")
+             {return{
+                country : prev.country,
+                address : value,
+                location : prev.location
+             }}
+             if(name === "map")
+             {return{
+                country : prev.country,
+                address : prev.address,
+                location : value
+             }}
+        });
+    }
+   return(
            <div className="addplace-container">
            <h2>Please Upload Your file:</h2>
-           <input type="file" id="myFile" name="filename" className="choose-file" />
-           <label for="myfile" className="label">choose file</label>
+           <input 
+                type="file" 
+                id="myFile" 
+                name="filename" 
+                className="choose-file" 
+            />
+           <label htmlFor="myfile" className="label">choose file</label>
            <br></br>
            <br></br>
+          
            <label htmlFor="contry">Country:</label>
-           <input type="text" className="contry" name="country" placeholder="Enter country"/>
+           <input 
+                type="text" 
+                className="contry" 
+                name="country" 
+                placeholder="Enter country" 
+                onChange ={InputHandler} 
+                value={Newvalue.Country}
+            />
+
            <label htmlFor="building">Building Name Or Address:</label>
-           <input type="text" name="building" placeholder="enter Location"/>
+           <input 
+                type="text" 
+                name="building" 
+                placeholder="enter Location"
+                onChange ={InputHandler} 
+                value={Newvalue.address} 
+              
+
+            />
            <label htmlFor="map">Location:</label>
-           <input type="text" name="map" placeholder="Enter Map location"/>
+           <input 
+                type="text" 
+                name="map" 
+                placeholder="Enter Map location"
+                onChange ={InputHandler} 
+                value={Newvalue.location}    
+                />
            <button type="submit" className="btn">ADD PLACE</button>
            </div>
         
--- src/Places/Myplaces/Places.js
+++ src/Places/Myplaces/Places.js
@@ -1,9 +1,11 @@
 import React from "react"
+import {useParams} from "react-router-dom"
 import Placeslist from "../Myplaces/Placeslist"
 import Logo1 from "../../Shared/images/Sydney_Opera_House_Front_angle (1).jpg"
+import Logo2 from "../../Shared/images/taj.jpg";
 
 const Places = () =>{
-    const Places = [ 
+    const DummyPlaces = [ 
         {
             id: 'p1',
             building: 'opera house',
@@ -12,11 +14,23 @@ const Places = () =>{
             address: '20 W 34th St,sydney, australia 10001',
             creator: 'u1'
 
-        }]
+        },
+        {
+            id: 'p2',
+            building: 'TAJ MAHAL',
+            statename: 'INDIA',
+            imgUrl:Logo2,
+            address: 'TAJ NAGRI, AGRA 282001',
+            creator: 'u2'
 
+        }
+    ];
+    const userid = useParams().userid;
+    const loadedplaces = DummyPlaces.filter(curr => curr.creator === userid);
     return(
-       <Placeslist items = {Places} />
+     
+       <Placeslist items = {loadedplaces} />
     );
 }
 
-export default Places
+export default Places;
