2bc4962bd82b2b1194915351a46448700f7f427b
shivank goyal <goyal.shivank790@gmail.com>
2021-06-30 23:09:11 +0530

    modal responsive

--- src/Shared/Modal/Modal.js
+++ src/Shared/Modal/Modal.js
@@ -7,7 +7,7 @@ const Modal = (props) =>{
             <div className="modal-container" style={{display : props.show ? "block" : "none" }}>
                 <h1 className="modal-header">{props.location}</h1>
                 <div className="map-container">
-                    <img src={Logo1} alt="map" />
+                    <img src={Logo1} alt="map" className="map-image"/>
                     <button className="map-button" onClick={props.onClick}>CLOSE</button>
                 </div>
             </div>,document.getElementById("modal-hook"));
