9b8640c154c8d3b85aa1bf4a81eebf041fb0045f
shivank goyal <goyal.shivank790@gmail.com>
2021-06-30 23:10:28 +0530

    usecontext render

--- src/Shared/Components/Navlinks.js
+++ src/Shared/Components/Navlinks.js
@@ -1,13 +1,23 @@
-import React from "react"
+import React, { useContext } from "react"
 import "./Navlinks.css"
 import  {NavLink}  from "react-router-dom"
+import { Authcontext } from "../Context/Authcontext"
 const Navlinks = () =>{
+    const Auth = useContext(Authcontext);
+    const logouthandler = () =>{
+        Auth.logout();
+    }
     return(
         <ul className="navlist">
         <li> <NavLink to="/" >ALL USERS</NavLink></li>
-        <li> <NavLink to="/u1/myplaces">MY PLACES</NavLink></li>
-         <li><NavLink to="/myplaces/new">ADD PLACES</NavLink></li>   
-         <li><NavLink to="/auth">LOGIN/SIGNUP</NavLink></li>   
+        {Auth.isLoggedIn && (
+        <li> <NavLink to="/u1/myplaces">MY PLACES</NavLink></li>)}
+        {Auth.isLoggedIn && (
+         <li><NavLink to="/myplaces/new">ADD PLACES</NavLink></li>)}
+         {!Auth.isLoggedIn && (   
+         <li><NavLink to="/auth">LOGIN/SIGNUP</NavLink></li>)}  
+        {Auth.isLoggedIn && (
+         <li><NavLink to="/" onClick={logouthandler}>LOG OUT</NavLink></li>)}
         </ul>
 
     );
