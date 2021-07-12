16b3121f337673147abbc1cb1b0b0ede868a568b
shivank goyal <goyal.shivank790@gmail.com>
2021-07-12 09:55:23 +0530

    express body parser

--- MERN/backend/app.js
+++ MERN/backend/app.js
@@ -1,9 +1,10 @@
 const express =require('express');
-const bodyParser = require("body-parser");
-
 const app = express();
 const placeroute = require("./Route/place-route");
 const userroute = require("./Route/users-route")
+
+
+app.use(express.json());
 app.use("/api",placeroute);
 app.use("/users",userroute);
 app.listen(5000);
