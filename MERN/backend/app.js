240493118e15b0fabad77949bef88936462d9ca1
shivank goyal <goyal.shivank790@gmail.com>
2021-07-27 23:03:39 +0530

    mongoose atlas connection string

--- MERN/backend/app.js
+++ MERN/backend/app.js
@@ -2,9 +2,17 @@ const express =require('express');
 const app = express();
 const placeroute = require("./Route/place-route");
 const userroute = require("./Route/users-route")
+const mongoose = require("mongoose");
 
 
 app.use(express.json());
+app.use((req,res,next) =>{
+res.setHeader('Access-Control-Allow-Origin','*');
+res.setHeader('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type,Accept,Authorization');
+res.setHeader('Access-Control-Allow-Methods','GET,POST,PATCH,DELETE');
+});
 app.use("/api",placeroute);
 app.use("/users",userroute);
-app.listen(5000);
+
+
+mongoose.connect("mongodb+srv://shivank:shivank123@cluster0.e3ldd.mongodb.net/places?retryWrites=true&w=majority").then(()=> {app.listen(5000);}).catch("cannot connect")
