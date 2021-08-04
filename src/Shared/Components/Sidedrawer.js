610542e7d3ff727656416b0b57a04e3fb929ff2e
shivank goyal <goyal.shivank790@gmail.com>
2021-08-04 21:59:14 +0530

    use authcontect in sidedrawer navs

--- src/Shared/Components/Sidedrawer.js
+++ src/Shared/Components/Sidedrawer.js
@@ -2,13 +2,18 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./Sidedrawer.css"
 import {Link} from "react-router-dom"
+import { useContext } from "react"
+import { Authcontext } from "../Context/Authcontext"
 const Sidedrawer = (props) =>{
+    const Auth = useContext(Authcontext);
    return ReactDOM.createPortal(
                 <div className="drawer" style={{display:props.show ? "block" : "none"}} onClick={props.onClick}>
                     <div className="drawerlist">
                         <div className="drawernav"><Link to="/">USERS</Link></div>
-                        <div className="drawernav"><Link to="/u1/myplaces">PLACES</Link></div>
-                        <div className="drawernav"><Link to="/myplaces/new">ADD PLACES</Link></div>
+                        {Auth.isLoggedIn &&
+                        <div className="drawernav"><Link to={`/${Auth.userId}/myplaces`}>MY PLACES</Link></div>}
+                        {Auth.isLoggedIn &&
+                        <div className="drawernav"><Link to="/myplaces/new">ADD PLACES</Link></div>}
                         <div className="drawernav"><Link to="/auth">LOGIN/SIGNUP</Link></div>
                     </div>
                 </div>,document.getElementById('sidedrawer-hook')
