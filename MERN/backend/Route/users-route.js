c3458816a62a97f8f71fe6fad1808fa28a579525
shivank goyal <goyal.shivank790@gmail.com>
2021-08-06 23:50:09 +0530

    call multer in signup route

--- MERN/backend/Route/users-route.js
+++ MERN/backend/Route/users-route.js
@@ -3,10 +3,11 @@ const users = express.Router();
 const {check} = require('express-validator');
 
 const usersControllers = require("../Controllers/Users-Controllers");
+const fileUpload = require('../middleware/file-upload');
 
 users.get('/',usersControllers.getusers);
 
-users.post("/signup",[check('email').normalizeEmail().isEmail() ,check('password').isLength({min:6}) ],usersControllers.signup);
+users.post("/signup",fileUpload.single('image'),[check('email').normalizeEmail().isEmail() ,check('password').isLength({min:6}) ],usersControllers.signup);
 
 users.post("/login",usersControllers.login);
 
