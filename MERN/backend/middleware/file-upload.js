0fb929e85409562393d4a7490aee59856e837839
shivank goyal <goyal.shivank790@gmail.com>
2021-08-07 23:58:22 +0530

    filtering images using multer

--- MERN/backend/middleware/file-upload.js
+++ MERN/backend/middleware/file-upload.js
@@ -1,19 +1,19 @@
 const multer = require('multer');
-const uuid = require('uuid/v1')
-const MIME_TYPE = {
+const { v1: uuid } = require('uuid')
+const MIME_TYPE_MAP = {
     'image/png' : 'png',
     'image/jpg' : 'jpg',
     'image/jpeg' : 'jpeg',
 
 }
 const fileUpload = multer({
-    limits : 500000,
+    limits : 5000000,
     storage : multer.diskStorage({
         destination : (req,file,cb)=>{
             cb(null,'uploads/images');
         },
         filename:(req,file,cb) =>{
-            const ext = MIME_TYPE(file.mimetype);
+            const ext = MIME_TYPE_MAP[file.mimetype];
             cb(null,uuid() + '.' + ext); 
         }   
     })
